c501d85fae8d4aaafdd95aeec1e3d526dee4b0f9
Mahmoud Abdulmajeed Taiye <mahmoudabdulmajeedtaiye@gmail.com>
2025-11-12 15:42:28 +0000

    cors fix

--- src/app.ts
+++ src/app.ts
@@ -33,6 +33,7 @@ app.use(
   })
 );
 
+
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -44,7 +45,6 @@ const limiter = rateLimit({
 });
 
 app.use("/api/contact", limiter, contactRoutes);
-
 app.get("/", (_req, res) => res.json({ status: "ok" }));
 
 export default app;
